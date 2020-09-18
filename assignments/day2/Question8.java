044d2a038180e5f4f46b124a6625b5e809e29495
fmyasar <71528998+fmyasar@users.noreply.github.com>
2020-09-19 01:17:23 +0300

    Update Question8.java

--- assignments/day2/Question8.java
+++ assignments/day2/Question8.java
@@ -1,5 +1,18 @@
-package assignments.day2;
+package day2_printing_comments;
 
-public class Question8 {
-
+public class task1_triangle {
+	
+	public static void main(String[] args) {
+		System.out.println("******          *************        **********");
+		System.out.println("**    **               **            **");
+		System.out.println("**     **              **            **");
+		System.out.println("**      **             **            **");
+		System.out.println("**      **             **            ********");
+		System.out.println("**      **      **     **            **");
+		System.out.println("**     **        **    **            **");
+		System.out.println("**    **          **  **             **");
+		System.out.println("*****              ****              **********");
+		
+		
+	}
 }
