9bd2c0e205a36fe473a3d6e5eb7656748e60e6f9
fmyasar <71528998+fmyasar@users.noreply.github.com>
2020-09-19 00:57:13 +0300

    Update Question6.java

--- assignments/day2/Question6.java
+++ assignments/day2/Question6.java
@@ -2,4 +2,20 @@ package assignments.day2;
 
 public class Question6 {
 
+	public static void main(String[] args) {
+		System.out.println("   ^");
+		System.out.println("  / \\");
+		System.out.println(" /   \\");
+		System.out.println("/     \\");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("-------");
+		
+	}
+	
 }
