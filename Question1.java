1734c2deaf9008ade2411985c18d5dba122250b6
fmyasar <71528998+fmyasar@users.noreply.github.com>
2020-09-22 01:23:40 +0300

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,15 @@
+package assignments2;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+
+		int count;
+		
+		count=3001;
+		
+		System.out.println(count);
+		
+	}
+
+}
---
+++ Question2.java
@@ -0,0 +1,16 @@
+package assignments2;
+
+public class Question2 {
+	
+	public static void main (String[]args) {
+		
+		String message="I am Fatih";
+		
+		System.out.println(message);
+		
+		
+		
+	
+	}	
+
+}
